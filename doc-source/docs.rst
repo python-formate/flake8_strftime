bae61fb88986d459f985cf1ce42839a516f22091
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-18 20:03:35 +0100

    Changed into package

---
+++ doc-source/docs.rst
@@ -0,0 +1,13 @@
+===========================
+API Reference
+===========================
+
+.. contents:: Table of Contents
+
+.. automodule:: flake8_strftime
+	:members:
+	:private-members:
+	:special-members:
+	:inherited-members:
+	:undoc-members:
+
--- flake8_strftime/__init__.py
+++ flake8_strftime/__init__.py
@@ -1,3 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  __init__.py
+"""
+A flake8 plugin which checks for use of platform specific strftime codes using .
+"""
+#
+#  Copyright (c) 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  Based on flake8_2020
+#  Copyright (c) 2019 Anthony Sottile
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+#
+
+__author__ = "Dominic Davis-Foster"
+__copyright__ = "2020 Dominic Davis-Foster"
+
+__license__ = "MIT"
+__version__ = "0.0.0"
+__email__ = "dominic@davis-foster.co.uk"
+
+
+
 import ast
 import re
 import sys
