6e71ef10cf57c4c69f82e73d79fc97fde3a3d638
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-17 20:32:33 +0100

    Updated config files.

--- setup.py
+++ setup.py
@@ -12,13 +12,10 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-
-
 setup(
 		description='A flake8 plugin which checks for use of platform specific strftime codes.',
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
 		version=__version__,
-
 		)
